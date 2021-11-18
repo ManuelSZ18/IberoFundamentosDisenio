9606ca8daa97963bbea37db8d42611bfadfb3203
ManuelSZ18 <manuelbuitrago18@gmail.com>
2021-11-18 11:56:49 -0500

    repositorioSOLID

---
+++ SOLID.js
@@ -0,0 +1,31 @@
+/* PRINCIPIO DE RESPONSABILIDAD UNICA
+    Este principio nos recomienda que una clase o funcion debe centrarse
+    especificamente en cumplir con una funcion y por tanto debe existir
+    una unica razon para cambiar.
+
+    Este principio exige que todos los metodos o subfunciones tengan alta cohesion.
+*/
+
+class bike {
+    constructor(brand, sizeFrame){
+    this.brand = brand;
+    this.sizeFrame = sizeFrame;
+    }
+
+    getBrand(){
+        return this.brand;
+    }
+
+    getSizeBrand(){
+        return this.sizeFrame;
+    }
+
+    saveBrand(){
+        this.brand;
+    }
+
+    saveSizeBrand(){
+        this.sizeFrame;
+    }
+}
+
