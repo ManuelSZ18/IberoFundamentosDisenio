192caa6b661201df3cdc5aa9bfcf0763f74efb61
ManuelSZ18 <manuelbuitrago18@gmail.com>
2021-11-18 19:53:12 -0500

    2do Principio SOLID

--- SOLID.js
+++ SOLID.js
@@ -28,3 +28,43 @@ class bike {
         this.sizeFrame;
     }
 }
+
+
+/* 
+
+PRINCIPIO DE ABIERTO/CERRADO
+    
+Es la capacidad de extender el comportamiento de una clase 
+o funcion sin modificar.
+
+*/
+
+class sizeFramesOnStock{
+
+    sizeFrames = ["S", "M", "L"];
+
+/*
+El indexOf devuelve la posicion del elemento en el array
+    
+Si retorna -1 es por que el elemento no existe y por tanto no tiene indice
+*/
+
+    OnStock(sizeFrame){
+        return this.sizeFrames.indexOf(sizeFrame) !== -1;
+    }
+
+/* Hasta aqui la clase contiene los tamaños disponibles
+    y a su vez, tiene una funcion que realiza una busqueda dentro
+    del array y retorna una información de acuerdo a lo busqueda realizada
+*/
+
+    pushSizeFrame(sizeFrame){
+        this.sizeFrames.push(sizeFrame);
+    }
+/*
+    Con esta ultima funcion, se logra realizar una modificacion 
+    a la clase, sin cambiar la funcionalidad que realizaba
+    anteriormente.
+*/
+
+}
